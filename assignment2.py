dd96fcac37c10a0a88b28b9b64a48e8e4d4a4a5a
Jackson <jackattack6279@gmail.com>
2019-02-25 11:42:03 -0700

    Start of assingment

---
+++ assignment2.py
@@ -0,0 +1,9 @@
+# Assignment 1 , Brady Morgan(30023858), Jackson Morphew (30068776), Jackson 50% Brady 50%
+# The purpose of this program is to allow the user to input information and recieve a graph of the information they input 
+# this graph is either in the form of a pie graph or a bar graph.
+
+# Importation of SimpleGraphics
+from SimpleGraphics import *
+
+#Constants
+PIECHART_DIAMETER=int(425)
